6843df165a17846c7af3a7a9730e5fbd5666eb66
Pankaj Kumar <pankaj.ducs@gmail.com>
2015-12-27 15:01:37 +0530

    temp commit.

--- LaBlog/lablog_app/models.py
+++ LaBlog/lablog_app/models.py
@@ -3,3 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserProfile(models.Model):
+  name = models.CharField(max_length=100)
+  about = models.CharField(max_length=1000)
+  background_image = models.CharField(max_length=500)
+  user_id = models.CharField(max_length=100)
+  password = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+  POST_STATUS = (
+      ('Draft', 'Draft'),
+      ('Published', 'Published')
+  )
+  heading = models.CharField(max_length=500)
+  sub_heading = models.CharField(max_length=1000)
+  content = models.CharField(max_length=2000)
+  background_image = models.CharField(max_length=500)
+  posted_by = models.CharField(max_length=100)
+  posted_on = models.CharField(max_length=50)
+  status = models.CharField(max_length=50, choices=POST_STATUS)
--- LaBlog/lablog_app/views.py
+++ LaBlog/lablog_app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
-from django.template import loader
+from django.core import serializers
 from django.http import HttpResponse
+from django.views.generic import View
 
-# Create your views here.
 
+class Blogs(View):
 
-def index(request):
-  template = loader.get_template('index.html')
-  return HttpResponse(template.render())
+  def get(self, request, *args, **kwargs):
+    index = request.GET.get('index', 0)
+    posted_by = request.GET.get('posted_by', 'all')
+
+    posts = None
+    if posted_by == 'all':
+      posts = Post.objects.all()[index:10]
+    else:
+      posts = Post.objects.filter(posted_by__exact=posted_by)
+    data = serializers.serialize('json', posts)
+    return HttpResponse(data, content_type='application/json')
+
+  def post(self, request, *args, **kwargs):
+    post_json =  request.POST.get('data', {})
+    post_obj = Post()
+    post_obj.save()
